ab00c7c3851f6a488ae4861dd6eb1ae25affb3a5
daveybo <dave.i.bowen@gmail.com>
2022-04-02 23:14:07 +0100

    datacharts

--- datacharts/main.py
+++ datacharts/main.py
@@ -1,34 +1,52 @@
+'''
+make sure to install pyqt5 and pyqtchart first
+'''
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout
-from PySide6.QtCharts import QChart, QChartView, QPieSeries, QPieSlice
-from PyQt5.QtGui import QPainter, QPen, QColor, QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtChart import QChart, QChartView, QPieSeries, QPieSlice
+from PyQt5.QtGui import QPainter, QPen
 from PyQt5.QtCore import Qt
 
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
-        super().__init__()
+
+        super(MainWindow, self).__init__()
 
         self.setGeometry(200, 200, 600, 400)
         self.setStyleSheet("background-color: blue;")
         self.setWindowTitle('Test Graphs')
 
-        self.show()
-
         self.create_pie_chart()
+
+        self.show()
 
     def create_pie_chart(self):
 
         # create pie series
+        pie_data = {
+            "fred": 220,
+            "james": 128,
+            "bob": 156,
+            "bill": 188
+        }
+
         series = QPieSeries()
-        for i in range(5):
-            text = str(i)
-            val = i
-            series.append(text, val)
-        # endfor
+        for key in pie_data:
+            series.append(key, pie_data[key])
+        # end for
 
         chart = QChart()
+
+        # adding slice
+        my_slice = QPieSlice()
+        my_slice = series.slices()[2]
+        my_slice.setExploded(True)
+        my_slice.setLabelVisible(True)
+        my_slice.setPen(QPen(Qt.darkGreen, 3))
+        my_slice.setBrush(Qt.green)
+
         chart.legend().hide()
         chart.addSeries(series)
         chart.setAnimationOptions(QChart.SeriesAnimations)
